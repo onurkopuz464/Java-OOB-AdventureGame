4dcaf1f07357a6f5ad083595018c3fae1c144cd8
onurkopuz464 <onurkopuz464@gmail.com>
2023-04-28 17:45:06 +0300

    fix character ori_

--- src/Entities/get_Character.java
+++ src/Entities/get_Character.java
@@ -23,9 +23,9 @@ public class get_Character {
         System.out.println("2-Paladin");
 
         Map<Integer, Character> player_Type = new HashMap<>();
-        player_Type.put(0, new Character(0, 35, 0, 0, 7));
-        player_Type.put(1, new Character(1, 30, 0, 0, 9));
-        player_Type.put(2, new Character(2, 45, 0, 0, 6));
+        player_Type.put(0, new Character(Character.Samurai.getMob_id(),Character.Samurai.getHealth(),Character.Samurai.Samurai.getShield(),Character.Samurai.getGold(),Character.Samurai.getDamage()));
+        player_Type.put(1, new Character(Character.Archer.getMob_id(),Character.Archer.getHealth(),Character.Archer.getShield(),Character.Archer.getGold(),Character.Archer.getDamage()));
+        player_Type.put(2, new Character(Character.Paladin.getMob_id(),Character.Paladin.getHealth(),Character.Paladin.getShield(),Character.Paladin.getGold(),Character.Paladin.getDamage()));
 
         player = player_Type.get(Utility.scan.scan());
         player_Main_Hp = player.getHealth();
